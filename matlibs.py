d21ec1bf9b7ff82f9772a5426e51b08d66b8d34b
unknown <yasirshab5@gmail.com>
2021-08-30 14:44:53 +0500

    matlibs

--- matlibs.py
+++ matlibs.py
@@ -1,1 +1,5 @@
-print('hello world')
+noun = input(" Please Enter a noun: ")
+verb = input(" Please Enter a verb here : ")
+celebrity_name = input(" Enter a celebrity name here : ")
+paragraph = "Today I was going for a " + verb + " but I encountered a " + noun + " so I stayed back and watched the movies of ad" +  celebrity_name
+print(paragraph)
